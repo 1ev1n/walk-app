63c3992c134b27acecc92886214091b68b47834c
dhy378jnd <doruleva@gmail.com>
2025-04-03 07:13:11 +0300

    DRAFTS

--- frontend/my-telegram-miniapp/src/api/axios.js
+++ frontend/my-telegram-miniapp/src/api/axios.js
@@ -4,12 +4,13 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
 const api = axios.create({
-    baseURL: baseURL,
+    baseURL: 'http://localhost:3000/api',  // Должно быть правильно настроено
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true, // Если требуется отправка куки
+    withCredentials: true,
 });
+
 
 // Проверка наличия Telegram WebApp
 const checkTelegramWebApp = () => {
